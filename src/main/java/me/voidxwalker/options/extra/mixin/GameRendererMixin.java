7a99460d49d64855bd673956fdc46e173d7c4494
moritz1029 <81555199+moritz1029@users.noreply.github.com>
2021-12-24 01:43:54 +0100

    Version 1.14

--- src/main/java/me/voidxwalker/options/extra/mixin/GameRendererMixin.java
+++ src/main/java/me/voidxwalker/options/extra/mixin/GameRendererMixin.java
@@ -1,23 +1,41 @@
 package me.voidxwalker.options.extra.mixin;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import me.voidxwalker.options.extra.GameOptionsAccess;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
     @Shadow @Final private MinecraftClient client;
-    @Redirect(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F",ordinal = 0))
+    @Shadow private int ticks;
+
+    @Redirect(method = "applyCameraTransformations", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F",ordinal = 0))
     public float extra_options_lerp(float delta, float start, float end){
         return MathHelper.lerp(delta, this.client.player.lastNauseaStrength, this.client.player.nextNauseaStrength) * ((GameOptionsAccess)this.client.options).extra_options_getDistortionEffectScale() * ((GameOptionsAccess)this.client.options).extra_options_getDistortionEffectScale();
+    }
+    @Inject(method = "applyCameraTransformations",at=@At(value="TAIL"))
+    public void e(float tickDelta, CallbackInfo ci){
+        if(MathHelper.lerp(tickDelta, this.client.player.lastNauseaStrength, this.client.player.nextNauseaStrength) * ((GameOptionsAccess)this.client.options).extra_options_getDistortionEffectScale() * ((GameOptionsAccess)this.client.options).extra_options_getDistortionEffectScale()==0){
+            int i = 20;
+            if (this.client.player.hasStatusEffect(StatusEffects.NAUSEA)) {
+                i = 7;
+            }
+
+            GlStateManager.rotatef(((float)this.ticks + tickDelta) * (float)i, 0.0F, 1.0F, 1.0F);
+            GlStateManager.scalef(1.0F , 1.0F, 1.0F);
+            GlStateManager.rotatef(-((float)this.ticks + tickDelta) * (float)i, 0.0F, 1.0F, 1.0F);
+        }
     }
     @Inject(method = "getFov",at = @At(value = "RETURN"),cancellable = true)
     private void extra_options_injected(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Double> cir) {
--- src/main/java/me/voidxwalker/options/extra/mixin/SplashScreenMixin.java
+++ src/main/java/me/voidxwalker/options/extra/mixin/SplashScreenMixin.java
@@ -1,31 +1,29 @@
 package me.voidxwalker.options.extra.mixin;
 
+import me.voidxwalker.options.extra.BackgroundHelper;
 import me.voidxwalker.options.extra.GameOptionsAccess;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.BackgroundHelper;
 import net.minecraft.client.gui.screen.Overlay;
 import net.minecraft.client.gui.screen.SplashScreen;
-import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.IntSupplier;
 
 @Mixin(SplashScreen.class)
 public abstract class SplashScreenMixin extends Overlay {
-    @Mutable @Shadow @Final private static int field_25042;
-    @Mutable @Shadow @Final private static int field_25041;
+    @Shadow @Final private MinecraftClient client;
     @Unique private static final int extra_options_MOJANG_RED = BackgroundHelper.ColorMixer.getArgb(255, 239, 50, 61);
     @Unique private static final int extra_options_MONOCHROME_BLACK = BackgroundHelper.ColorMixer.getArgb(255, 0, 0, 0);
     @Unique private static final IntSupplier extra_options_BRAND_ARGB = () -> ((GameOptionsAccess)MinecraftClient.getInstance().options).extra_options_getMonochromeLogo()? extra_options_MONOCHROME_BLACK : extra_options_MOJANG_RED;
-    @Inject(
-            method = {"render"},
-            at = @At(value = "HEAD")
-    )
-    public void extra_options_render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci){
-        field_25042=extra_options_BRAND_ARGB.getAsInt();
-        field_25041=extra_options_BRAND_ARGB.getAsInt();
+    @Inject(method = "render",at=@At(value="INVOKE",target = "Lnet/minecraft/client/util/Window;getScaledWidth()I" ,shift = At.Shift.AFTER,ordinal = 1))
+    public void fill(int mouseX, int mouseY, float delta, CallbackInfo ci){
+        int i = this.client.window.getScaledWidth();
+        int j = this.client.window.getScaledHeight();
+        fill(0, 0, i, j, extra_options_BRAND_ARGB.getAsInt());
     }
+
 }
