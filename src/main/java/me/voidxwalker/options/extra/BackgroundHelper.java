8dd998791060f27dde833d57a30c341ccbd65650
Wurgo <71964154+wurgo@users.noreply.github.com>
2022-01-26 09:02:25 +0100

    Update BackgroundHelper.java

--- src/main/java/me/voidxwalker/options/extra/BackgroundHelper.java
+++ src/main/java/me/voidxwalker/options/extra/BackgroundHelper.java
@@ -2,10 +2,6 @@ package me.voidxwalker.options.extra;
 
 public class BackgroundHelper {
     public static class ColorMixer {
-
-        public static int getArgb(int alpha, int red, int green, int blue) {
-            return alpha << 24 | red << 16 | green << 8 | blue;
-        }
-
+        public static int getArgb(int alpha, int red, int green, int blue) { return alpha << 24 | red << 16 | green << 8 | blue; }
     }
 }
