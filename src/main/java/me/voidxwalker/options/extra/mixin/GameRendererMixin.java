3310075b62d4dddd52edded7bd682481284cbcee
Wurgo <71964154+wurgo@users.noreply.github.com>
2022-01-26 09:42:55 +0100

    Update GameRendererMixin.java

--- src/main/java/me/voidxwalker/options/extra/mixin/GameRendererMixin.java
+++ src/main/java/me/voidxwalker/options/extra/mixin/GameRendererMixin.java
@@ -20,28 +20,24 @@ public class GameRendererMixin {
     @Shadow @Final private MinecraftClient client;
     @Shadow private int ticks;
 
-    @Redirect(method = "applyCameraTransformations", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F",ordinal = 0))
-    public float extra_options_lerp(float delta, float start, float end){
-        return MathHelper.lerp(delta, this.client.player.lastNauseaStrength, this.client.player.nextNauseaStrength) * ((GameOptionsAccess)this.client.options).extra_options_getDistortionEffectScale() * ((GameOptionsAccess)this.client.options).extra_options_getDistortionEffectScale();
-    }
-    @Inject(method = "applyCameraTransformations",at=@At(value="TAIL"))
-    public void e(float tickDelta, CallbackInfo ci){
-        if(MathHelper.lerp(tickDelta, this.client.player.lastNauseaStrength, this.client.player.nextNauseaStrength) * ((GameOptionsAccess)this.client.options).extra_options_getDistortionEffectScale() * ((GameOptionsAccess)this.client.options).extra_options_getDistortionEffectScale()==0){
+    @Redirect(method = "applyCameraTransformations", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F", ordinal = 0))
+    public float extra_options_lerp(float delta, float start, float end) { return MathHelper.lerp(delta, this.client.player.lastNauseaStrength, this.client.player.nextNauseaStrength) * ((GameOptionsAccess) this.client.options).extra_options_getDistortionEffectScale() * ((GameOptionsAccess) this.client.options).extra_options_getDistortionEffectScale(); }
+    
+    @Inject(method = "applyCameraTransformations", at=@At(value="TAIL"))
+    public void e(float tickDelta, CallbackInfo ci) {
+        if (MathHelper.lerp(tickDelta, this.client.player.lastNauseaStrength, this.client.player.nextNauseaStrength) * ((GameOptionsAccess) this.client.options).extra_options_getDistortionEffectScale() * ((GameOptionsAccess) this.client.options).extra_options_getDistortionEffectScale() == 0) {
             int i = 20;
-            if (this.client.player.hasStatusEffect(StatusEffects.NAUSEA)) {
-                i = 7;
-            }
+            if (this.client.player.hasStatusEffect(StatusEffects.NAUSEA)) { i = 7; }
 
-            GlStateManager.rotatef(((float)this.ticks + tickDelta) * (float)i, 0.0F, 1.0F, 1.0F);
+            GlStateManager.rotatef(((float) this.ticks + tickDelta) * (float) i, 0.0F, 1.0F, 1.0F);
             GlStateManager.scalef(1.0F , 1.0F, 1.0F);
-            GlStateManager.rotatef(-((float)this.ticks + tickDelta) * (float)i, 0.0F, 1.0F, 1.0F);
+            GlStateManager.rotatef(-((float) this.ticks + tickDelta) * (float) i, 0.0F, 1.0F, 1.0F);
         }
     }
-    @Inject(method = "getFov",at = @At(value = "RETURN"),cancellable = true)
+    
+    @Inject(method = "getFov", at = @At(value = "RETURN"), cancellable = true)
     private void extra_options_injected(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Double> cir) {
         FluidState fluidState = camera.getSubmergedFluidState();
-        if (!fluidState.isEmpty()) {
-            cir.setReturnValue((cir.getReturnValue()*70D/60D)*(double)MathHelper.lerp(((GameOptionsAccess)this.client.options).extra_options_getFOVEffectScale(), 1.0F, 0.85714287F));
-        }
+        if (!fluidState.isEmpty()) { cir.setReturnValue((cir.getReturnValue() * 70D / 60D) * (double) MathHelper.lerp(((GameOptionsAccess) this.client.options).extra_options_getFOVEffectScale(), 1.0F, 0.85714287F)); }
     }
 }
